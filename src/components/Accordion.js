73fcb1f6d1bf2d5bc6d5b9bba614dad5f2b98715
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-11-13 20:08:34 +0430

    handling title clicked

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const onTitleClicked = (index) => {
+    console.log("Item clicked", index);
+  };
+  const renderedItems = items.map((item, index) => {
     return (
-      <div key={item.title}>
-        <div className="title active">
+      <React.Fragment key={item.title}>
+        <div className="title active" onClick={() => onTitleClicked(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
         <div className="content active">
           <p>{item.content}</p>
         </div>
-      </div>
+      </React.Fragment>
     );
   });
   return <div className="ui styled accordion">{renderedItems}</div>;
