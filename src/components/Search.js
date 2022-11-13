293e5d98f40d7d001019fea497429e4323e632a0
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-11-13 23:09:26 +0430

    create Search box to search terms

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [term, setTerm] = useState("");
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Search;
