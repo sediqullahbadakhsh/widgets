5be8ef731095bbb86e786baf116e3271ac09bf7e
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-11-17 02:07:43 +0430

    list search result

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,6 +20,16 @@ const Search = () => {
     };
     search();
   }, [term]);
+  const renderResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
       <div className="ui form">
@@ -32,6 +42,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderResults}</div>
     </div>
   );
 };
