4aa3d8044d1a7f762f6e303a91e44ae8a63c7b7d
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-11-17 13:26:43 +0430

    update dropdown select option

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,7 +3,11 @@ import { render } from "@testing-library/react";
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
     return (
-      <div key={option.value} className="item">
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+      >
         {option.label}
       </div>
     );
@@ -14,7 +18,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <label className="label">Select a Color</label>
         <div className="ui selection dropdown visible active">
           <i className="dropdown icon"></i>
-          <div className="text">{selected.lable}</div>
+          <div className="text">{selected.label}</div>
           <div className="menu visible transition">{renderedOptions}</div>
         </div>
       </div>
